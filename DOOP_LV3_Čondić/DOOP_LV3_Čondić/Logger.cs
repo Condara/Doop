c322a4e382805f0fb0d2d290caf7a7beae4e7040
Condara <56083206+Condara@users.noreply.github.com>
2020-05-06 19:20:24 +0200

    DOOP_LV3_Čondić

---
+++ DOOP_LV3_Čondić/DOOP_LV3_Čondić/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOOP_LV3_Čondić
+{
+    class Logger
+    {
+        private static Logger instance;
+        private string filePath;
+        private Logger(string filePath)
+        {
+            this.filePath = filePath;
+        }
+        public static Logger GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Logger("Logger.txt");
+            }
+            return instance;
+        }
+        public void Log(string data)
+        {
+            using (System.IO.StreamWriter writer = new System.IO.StreamWriter(this.filePath, append: true))
+            {
+                writer.WriteLine("\n\n" + data);
+            }
+        }
+    }
+}
---
+++ DOOP_LV3_Čondić/DOOP_LV3_Čondić/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOOP_LV3_Čondić
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset newDS = new Dataset("LV3.txt");
+            Console.WriteLine(newDS.DatasetOutput());
+            MatrixGenerator matrix = MatrixGenerator.GetInstance();
+            matrix.MatrixOutput();
+            Notification noticeAlert = new Notification("Autor a", "Naslov", "ALERT", DateTime.Now, Category.ALERT, ConsoleColor.DarkYellow);
+            Notification noticeError = new Notification("Autor b", "Naslov", "ERROR", DateTime.Now, Category.ERROR, ConsoleColor.DarkRed);
+            Notification noticeInfo = new Notification("Autor c", "Naslov", "INFO", DateTime.Now, Category.INFO, ConsoleColor.Green);
+            NotificationManager noticeManager = new NotificationManager();
+            noticeManager.Display(noticeAlert);
+            noticeManager.Display(noticeError);
+            noticeManager.Display(noticeInfo);
+            NotificationBuilder builder = new NotificationBuilder();
+            Director director = new Director();
+            director.Construct(builder, "Autor");
+            Notification noticeBuilt = builder.Build();
+            noticeManager.Display(noticeBuilt);
+        }
+    }
+}
+
